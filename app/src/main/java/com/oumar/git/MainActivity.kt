369853877ebf22062415801a8d26387117b51f19
lab_services_student <ST10444655@vcconnect.edu.za>
2024-03-18 10:01:07 +0200

    My first android App 3.0

---
+++ app/src/main/java/com/oumar/git/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.oumar.git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //declare a variable and initialize
+    private val birthdate = 15
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        findViewById<View>(R.id.button).set
+    }
+}
